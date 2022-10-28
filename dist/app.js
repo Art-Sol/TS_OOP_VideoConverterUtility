d1791a9895955e0dd38c3354d2c2d2d56906d037
Art-Sol <solodushenkov-sp@mail.ru>
2022-10-28 17:53:18 +0300

    finished project

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-class App {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { FfmpegExecutor } from "./commands/ffmpeg/ffmpeg.executor.js";
+import { ConsoleLogger } from "./out/console-logger/console-logger.js";
+export class App {
     run() {
-        console.log('done');
+        return __awaiter(this, void 0, void 0, function* () {
+            new FfmpegExecutor(ConsoleLogger.getInstance()).execute();
+        });
     }
 }
-exports.App = App;
 const app = new App();
 app.run();
